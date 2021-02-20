7b9c51e44045405d974ea956a167ef01a0896415
carlos23fb <jcflobe@gmail.com>
2021-02-19 21:55:05 -0700

    Path parameters containing paths

--- firstSteps/main.py
+++ firstSteps/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from enum import Enum
 # TODO Importar fastApi
 
 # TODO Crear una instancia
 app = FastAPI()
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 @app.get("/")
 async def root():
     return {"message" : "Hello World!!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "usuario actual"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
