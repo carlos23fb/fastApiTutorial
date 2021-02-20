e9a57cb255b0f1a99f307961080259e383c0fb67
carlos23fb <jcflobe@gmail.com>
2021-02-20 16:47:34 -0700

    Required query params

--- firstSteps/main.py
+++ firstSteps/main.py
@@ -66,11 +66,18 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
 
 # TODO Multiple path and query params
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "This is an amazing item that has a long description"})
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "This is an amazing item that has a long description"})
+#     return item
+
+# TODO Required query params
+
+@app.get("/item/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item= {"item_id": item_id, "needy": needy}
     return item
