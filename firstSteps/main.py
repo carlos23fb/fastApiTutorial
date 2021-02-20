05f2f7c4d56d7cbc3d9b977e85f954a6c1962c89
carlos23fb <jcflobe@gmail.com>
2021-02-20 16:37:20 -0700

    Multiple path and query params

--- firstSteps/main.py
+++ firstSteps/main.py
@@ -63,3 +63,14 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
     if not short:
         item.update({"short": "This is amazing item has a long description"})
     return item
+
+# TODO Multiple path and query params
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
