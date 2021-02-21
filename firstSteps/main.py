8d789d2f9f29fc7940ee2bd286c0b74de061029e
carlos23fb <jcflobe@gmail.com>
2021-02-20 17:06:52 -0700

    Optional and default query params

--- firstSteps/main.py
+++ firstSteps/main.py
@@ -77,7 +77,12 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
 
 # TODO Required query params
 
+# @app.get("/item/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item= {"item_id": item_id, "needy": needy}
+#     return item
+
 @app.get("/item/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item= {"item_id": item_id, "needy": needy}
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
