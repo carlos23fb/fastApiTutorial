2638ceb31f5489a708a7c4cc8d5153df572a5f2d
Carlos <jcflobe@gmail.com>
2021-02-22 10:44:09 -0700

    Multiple body parameters

--- firstSteps/main.py
+++ firstSteps/main.py
@@ -24,21 +24,38 @@ class Item(BaseModel):
     price: float
     tax: Optional[float] = None
 
-# TODO Mix Path, Query and body parameters
+class User(BaseModel):
+    user_name: str
+    full_name: Optional[str] = None
+
+
+# TODO Multiple body parameters
 
 @app.put("/items/{item_id}")
 async def update_items(
-    *,
-    item_id: int = Path(..., title="The Id of the item to get", ge=0, le=1000),
-    q: Optional[str] = None,
-    item: Optional[Item] = None
+    item_id: int,
+    item: Item,
+    user: User
 ):
-    result = {"item_id": item_id}
-    if q:
-        result.update({"q": q})
-    if item:
-        result.update({"item": item})
+    result ={"item_id": item_id, "item": item, "user": user}
     return result
+
+
+# TODO Mix Path, Query and body parameters
+
+# @app.put("/items/{item_id}")
+# async def update_items(
+#     *,
+#     item_id: int = Path(..., title="The Id of the item to get", ge=0, le=1000),
+#     q: Optional[str] = None,
+#     item: Optional[Item] = None
+# ):
+#     result = {"item_id": item_id}
+#     if q:
+#         result.update({"q": q})
+#     if item:
+#         result.update({"item": item})
+#     return result
 
 
 
