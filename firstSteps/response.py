9d51b85a935425181eef1c591a46b2599ac88e63
Carlos <jcflobe@gmail.com>
2021-02-25 20:03:16 -0700

    Use the response_model_exclude_unset parameter

--- firstSteps/response.py
+++ firstSteps/response.py
@@ -5,12 +5,22 @@ from pydantic import BaseModel, EmailStr
 app = FastAPI()
 
 
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []}
+}
+
+
+
+
+
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
-    tax: Optional[float] = None
-    tags: Optional[List[str]] = []
+    tax: float = 10.5
+    tags: List[str] = []
 
 
 class UserIn(BaseModel):
@@ -41,3 +51,10 @@ async def create_item(item: Item):
 @app.post("/user/", response_model=UserOut)
 async def create_user(user: UserIn):
     return user
+
+
+
+
+@app.get("/item/read/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
